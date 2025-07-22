56ac6e10c23c748ccabe50fc64a466b6570814ca
janevskistefan <38108655+janevskistefan@users.noreply.github.com>
2025-07-22 20:28:13 +0200

    introduce spring boot project

---
+++ server/src/test/java/com/janevskistefan/public_transport_manager/PublicTransportManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janevskistefan.public_transport_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicTransportManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
